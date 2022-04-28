ea727d8df8ce32f6923aa182bc1c1ab05b5ee44d
mangelicaeh <68961085+mangelicaeh@users.noreply.github.com>
2022-04-28 11:28:53 -0400

    Add files via upload

---
+++ charts/scatter3d.go
@@ -0,0 +1,32 @@
+package charts
+
+import (
+	"github.com/mangelicaeh/go-machart/opts"
+	"github.com/mangelicaeh/go-machart/render"
+	"github.com/mangelicaeh/go-machart/types"
+	/*"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/render"
+	"github.com/go-echarts/go-echarts/v2/types"*/)
+
+// Scatter3D represents a 3D scatter chart.
+type Scatter3D struct {
+	Chart3D
+}
+
+// Type returns the chart type.
+func (Scatter3D) Type() string { return types.ChartScatter3D }
+
+// NewScatter3D creates a new 3D scatter chart.
+func NewScatter3D() *Scatter3D {
+	c := &Scatter3D{}
+	c.initBaseConfiguration()
+	c.Renderer = render.NewChartRender(c, c.Validate)
+	c.initChart3D()
+	return c
+}
+
+// AddSeries adds the new series.
+func (c *Scatter3D) AddSeries(name string, data []opts.Chart3DData, options ...SeriesOpts) *Scatter3D {
+	c.addSeries(types.ChartScatter3D, name, data, options...)
+	return c
+}
