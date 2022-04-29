be65cb1d58999a6aa03293f6fc385d5e97b3ad2c
mangelicaeh <68961085+mangelicaeh@users.noreply.github.com>
2022-04-29 12:37:18 -0400

    Update global.go

--- opts/global.go
+++ opts/global.go
@@ -1114,22 +1114,16 @@ type ZAxis3D struct {
 type Grid struct {
 	// Height
 	//Height float32 `json:"height,omitempty"`
-
 	// Bottom
 	//Bottom float32 `json:"bottom,omitempty"`
-	
 	// Height
 	Height float32 `json:"height,omitempty"`
-
 	// Distance between grid component and the bottom side of the container.
 	Bottom string `json:"bottom,omitempty"`
-	
 	// Distance between grid component and the left side of the container.
 	Left string `json:"left,omitempty"`
-
 	// Distance between grid component and the right side of the container.
 	Right string `json:"right,omitempty"`
-
 	// Distance between grid component and the top side of the container.
 	Top string `json:"top,omitempty"`
 }
